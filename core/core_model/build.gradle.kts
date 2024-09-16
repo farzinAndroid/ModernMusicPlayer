0deaab65e9cb41f48a5bc18161191b45a9ba9fb5
Farzin <farzinallahyari1234@gmail.com>
2024-09-16 13:32:20 +0330

    update gradle

--- core/core_model/build.gradle.kts
+++ core/core_model/build.gradle.kts
@@ -34,5 +34,12 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
     api(libs.kotlinx.datetime)
 }
