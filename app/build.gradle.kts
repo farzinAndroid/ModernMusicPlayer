552fd7fa0f362a6d4708d7e9a903cb0c4c41f60c
Farzin <farzinallahyari1234@gmail.com>
2025-03-22 15:56:57 +0330

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.farzin.modernmusicplayer"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 10
-        versionName = "1.3.6"
+        versionCode = 11
+        versionName = "1.3.7"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
